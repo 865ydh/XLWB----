5535f35e99ecf51d8f57303dba9fe9e18c8516e7
闫栋辉 <YDH@yandonghuideMacBook-Pro.local>
2014-05-05 19:13:47 +0800

    Initial Commit

---
+++ XLWB/YDHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YDHAppDelegate.h
+//  XLWB
+//
+//  Created by 闫栋辉 on 14-5-5.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YDHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XLWB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XLWB
+//
+//  Created by 闫栋辉 on 14-5-5.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YDHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YDHAppDelegate class]));
+    }
+}
